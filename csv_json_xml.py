f2580ea4ef9b00a6f5b6d9b9f3a003044c6fc368
zach.zhang <zach.zhang@quectel.com>
2020-05-27 16:33:46 +0800

    add excel

--- csv_json_xml.py
+++ csv_json_xml.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xlrd
 from xml.etree import ElementTree as ET
 
 def csv_print(file_name):
@@ -48,9 +49,15 @@ def xml_print(file_name):
                                     rec_value = nv.text
                                 record[rec_key] = rec_value
                 all_data.append(record)
-
     print(all_data)
+
+def excel_print(file_name, sheet_name):
+    book = xlrd.open_workbook(file_name)
+    sheet = book.sheet_by_name(sheet_name)
+    for i in range(sheet.nrows):
+        print(sheet.row_values(i))
 
 #json_print("C:\\Users\\zach.zhang\\Desktop\\statistics-config.json")
 #csv_print("C:\\Users\\zach.zhang\\Desktop\\_cache.csv")
 #xml_print("C:\\Users\\zach.zhang\\Desktop\\0518_NV_New.xml")
+#excel_print("C:\\Users\\zach.zhang\\Desktop\\1234.xlsx","Sheet1")
