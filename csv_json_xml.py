7a66d210fd7896bba3bdb99e3371f772c84153dd
Zach Zhang <38149088+zach341@users.noreply.github.com>
2020-05-27 13:40:59 +0800

    add csv_json_xml.py

---
+++ csv_json_xml.py
@@ -0,0 +1,56 @@
+import csv
+import json
+from xml.etree import ElementTree as ET
+
+def csv_print(file_name):
+    csvfile = open(file_name, 'r')
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
+
+def json_print(file_name):
+    json_data = open(file_name).read()
+    data = json.loads(json_data)
+    for key, value in data.items():
+        if isinstance(value, list):
+            print(key + ': ')
+            for key_l, value_l in value[0].items():
+                print('\t{}: {}'.format(key_l, value_l))
+        elif isinstance(value, dict):
+            print(key + ': ')
+            for key_t, value_t in value.items():
+                print('\t{}: {}'.format(key_t, value_t))
+        else:
+            print('{}: {}'.format(key,value))
+
+def xml_print(file_name):
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+    NvSource = root.find('NvSource')
+
+    all_data = []
+    for product_NvInfo in NvSource:
+        lookup_key = product_NvInfo.attrib.keys()
+        record = {}
+        if list(lookup_key)[0]== 'MODEM_NUMBER':
+            rec_value = product_NvInfo.attrib['MODEM_NUMBER']
+            if rec_value == '1':
+                for product in product_NvInfo:
+                    lookup_key_pro = product.attrib.keys()
+                    print(lookup_key_pro)
+                    if list(lookup_key_pro)[0] == 'product_id':
+                        id_value = product.attrib['product_id']
+                        if id_value == '0':
+                            for nv in product:
+                                lookup_key_nv = nv.attrib.keys()
+                                if list(lookup_key_nv)[0] == 'id':
+                                    rec_key = nv.attrib['id']
+                                    rec_value = nv.text
+                                record[rec_key] = rec_value
+                all_data.append(record)
+
+    print(all_data)
+
+#json_print("C:\\Users\\zach.zhang\\Desktop\\statistics-config.json")
+#csv_print("C:\\Users\\zach.zhang\\Desktop\\_cache.csv")
+#xml_print("C:\\Users\\zach.zhang\\Desktop\\0518_NV_New.xml")
